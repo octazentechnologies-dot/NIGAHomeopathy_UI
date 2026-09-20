e13f952c55a8723fc48993e811138b99d280aee1
GouravNikam11 <gouravnikam11@gmail.com>
2026-09-20 17:19:17 +0530

    changes in config file

--- src/config.js
+++ src/config.js
@@ -17,21 +17,21 @@
 //   }
 // };
 
-module.exports = {
-  api: {
-    //API_URL_NIGAHOMEOPATHY: "https://api.nigahomeopathy.com/api",
-    //API_URL_NIGAHOMEOPATHY: "https://api1.homeocentrum.com/api",
-    //API_URL: "https://api.homeocentrum.com/api",
-    //API_URL: "https://api-node.themesbrand.website",
-    API_URL_NIGAHOMEOPATHY: "http://localhost:5038/api",
-    API_URL: "http://localhost:5001/api",
-  }
-};
-
-
 // module.exports = {
 //   api: {
-//     API_URL_NIGAHOMEOPATHY: "https://devapi2.homeocentrum.com/api", // this is for development use 
-//     API_URL: "https://devapi1.homeocentrum.com/api",  // api1 use for production only 
+//     //API_URL_NIGAHOMEOPATHY: "https://api.nigahomeopathy.com/api",
+//     //API_URL_NIGAHOMEOPATHY: "https://api1.homeocentrum.com/api",
+//     //API_URL: "https://api.homeocentrum.com/api",
+//     //API_URL: "https://api-node.themesbrand.website",
+//     API_URL_NIGAHOMEOPATHY: "http://localhost:5038/api",
+//     API_URL: "http://localhost:5001/api",
 //   }
 // };
+
+
+module.exports = {
+  api: {
+    API_URL_NIGAHOMEOPATHY: "https://devapi2.homeocentrum.com/api", // this is for development use 
+    API_URL: "https://devapi1.homeocentrum.com/api",  // api1 use for production only 
+  }
+};
